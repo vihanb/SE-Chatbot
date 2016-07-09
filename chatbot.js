7c03370a05fe19081eda323bb1ca7dfa15255cc5
Vihan <vihanb@users.noreply.github.com>
2016-07-09 09:53:44 -0700

    Added !!zgoat

--- chatbot.js
+++ chatbot.js
@@ -1,85 +1,36 @@
-"use strict";
+"use strict";function _toConsumableArray(t){if(Array.isArray(t)){for(var e=0,n=Array(t.length);e<t.length;e++)n[e]=t[e];return n}return Array.from(t)}function _classCallCheck(t,e){if(!(t instanceof e))throw new TypeError("Cannot call a class as a function")}var Talk=function(t){document.getElementById("input").value=t,document.getElementById("sayit-button").click()},Data=function(t,e){return t.replace(/\$([A-Za-z$_]+[A-Za-z$_0-9]*)/g,function(t,n){return e[n]})},Chatbot=function t(){var e=arguments.length<=0||void 0===arguments[0]?"a Chatbot":arguments[0],n=this,a=arguments.length<=1||void 0===arguments[1]?{}:arguments[1],r=a.Startup,u=void 0===r?"Hi my name is $Name!":r,s=arguments[2];_classCallCheck(this,t);var i=CHAT.CURRENT_USER_ID;this.Name=e,this.Options={Startup:Data(u,this),UID:i},this.onmessage=s||function(){},this.Queue=[];var o=Symbol("Called");this[o]=new Set,Talk(this.Options.Startup),setInterval(function(){Array.from(document.getElementsByClassName("message")).filter(function(t,e,a){return n[o].has(t)?!1:(n[o].add(t),(t.parentElement.parentElement.className.match(/user-(\d+)/)||[0,i])[1]!=i&&e>a.map(function(t){return t.textContent.trim()}).lastIndexOf(n.Options.Startup))}).forEach(function(t){return n.onmessage.call({Text:t.textContent.trim(),HTML:t.innerHTML,Raw:t,User:t.parentElement.parentElement.getElementsByClassName("username")[0].textContent.trim(),Mentions:[].concat(_toConsumableArray(t.querySelectorAll(".mention"))).map(function(t){return(t.textContext||"").slice(1)}),Speak:function(t){return n.Queue.push(t)},Reply:function(e){return n.Queue.push(":"+t.id.split("-")[1]+" "+e)},Star:function(e){return t.querySelector(".meta > .stars > .vote").click()},"super":n})})},2100),setInterval(function(){n.Queue[0]&&Talk(n.Queue.shift())},2500)};
 
-function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+var DICTIONARY = new Map([ // Define commands in this object
+	["!!zhelp", "No help yet, but you can use !!zlistcommands."],
+	["!!zlistcommands", "Commands: listcommands, alive"],
+	["!!zalive", "Watching this endless feed of new messages never gets boring."],
+	["!!zgoat", function(self) {
+		// Im sorry about jQuery but I just quickly found this on SO
+		$(document).ready(function(){
+        		$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
+        			tags: "goat",
+        			tagmode: "any",
+        			format: "json"
+        		},
+        		function(data) {
+            			var rnd = Math.floor(Math.random() * data.items.length);
+            			var image_src = data.items[rnd]['media']['m'].replace("_m", "_b");
+            			self.Reply(image_src);
+			});
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Talk = function Talk(text) {
-  document.getElementById("input").value = text;document.getElementById("sayit-button").click();
-};
-var Data = function Data(text, instance) {
-  return text.replace(/\$([A-Za-z$_]+[A-Za-z$_0-9]*)/g, function (_, v) {
-    return instance[v];
-  });
-};
-
-var Chatbot = function Chatbot() {
-  var Name = arguments.length <= 0 || arguments[0] === undefined ? "a Chatbot" : arguments[0];
-
-  var _this = this;
-
-  var _ref = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
-
-  var _ref$Startup = _ref.Startup;
-  var Startup = _ref$Startup === undefined ? "Hi my name is $Name!" : _ref$Startup;
-  var onmessage = arguments[2];
-
-  _classCallCheck(this, Chatbot);
-
-  var UID = CHAT.CURRENT_USER_ID;
-
-  this.Name = Name;
-  this.Options = { Startup: Data(Startup, this), UID: UID };
-  this.onmessage = onmessage || function () {};
-
-  this.Queue = [];
-  // Private
-  var Called = Symbol("Called");
-
-  this[Called] = new Set();
-
-  // Startup
-  Talk(this.Options.Startup);
-
-  // Read
-  setInterval(function () {
-    Array.from(document.getElementsByClassName("message")).filter(function (message, index, transcript) {
-      if (_this[Called].has(message)) return false;else _this[Called].add(message);
-      return (message.parentElement.parentElement.className.match(/user-(\d+)/) || [0, UID])[1] != UID && index > transcript.map(function (t) {
-        return t.textContent.trim();
-      }).lastIndexOf(_this.Options.Startup);
-    }).forEach(function (message) {
-      return _this.onmessage.call({
-        "Text": message.textContent.trim() /*.replace(/\b[a-z\.]+(?:com|org|net|xyz)/g, "http://$&")*/
-        , "HTML": message.innerHTML,
-        "Raw": message,
-
-        "User": message.parentElement.parentElement.getElementsByClassName("username")[0].textContent.trim(),
-
-        "Mentions": [].concat(_toConsumableArray(message.querySelectorAll(".mention"))).map(function (m) {
-          return (m.textContext || "").slice(1);
-        }),
-
-        "Speak": function Speak(Text) {
-          return _this.Queue.push(Text);
-        },
-        "Reply": function Reply(Text) {
-          return _this.Queue.push(":" + message.id.split("-")[1] + " " + Text);
-        },
-        "Star": function Star(Text) {
-          return message.querySelector(".meta > .stars > .vote").click();
-        },
-
-        "super": _this
-      });
-    });
-  }, 2100);
-
-  setInterval(function () {
-    if (_this.Queue[0]) Talk(_this.Queue.shift());
-  }, 2500);
-};
+    		});
+	}]
+]);
 
 var MyChatbot = new Chatbot("a Chatbot", {Startup: "Hi my name is $Name!"}, function() {
-	
+	if(/hello/i.test(this.Text)) { this.Reply("Hello!"); }
+	else if(this.Text.slice(0, 3) == "!!z") {
+		var text = DICTIONARY.get(this.Text);
+		if (typeof text === 'function') {
+			// Call the function and pass this as arg and set as scope
+			text.call(this, this);
+		} else {
+			this.Reply(text || "Unknown command")
+		}
+	}
 });
